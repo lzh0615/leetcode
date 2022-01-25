b479be1cd75d6f0b684dbb83966e498224eab26a
Lzh <nordglede@outlook.com>
2022-01-26 00:31:22 +0800

    add: other-test

--- hot100/003-lengthOfLongestSubstring.js
+++ hot100/003-lengthOfLongestSubstring.js
@@ -3,18 +3,31 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  const arr = []
-  let max = 0
-  for (let s1 of s) {
-    if (arr.indexOf(s1) === -1) {
-      arr.push(s1)
-    } else {
-      max = Math.max(max, arr.length)
-      arr.splice(0, arr.indexOf(s1) + 1)
-      arr.push(s1)
-    }
+  // const arr = []
+  // let max = 0
+  // for (let s1 of s) {
+  //   if (arr.indexOf(s1) === -1) {
+  //     arr.push(s1)
+  //   } else {
+  //     max = Math.max(max, arr.length)
+  //     arr.splice(0, arr.indexOf(s1) + 1)
+  //     arr.push(s1)
+  //   }
+  // }
+  // return Math.max(max, arr.length)
+  let len = s.length;
+  let l = 0;
+  let ans = 0;
+  let i = 0;
+  for (; i < len; i++) {
+      let pos = s.indexOf(s[i], l);
+      // 从 [l, i] 区间如果有字符和当前字符重复，则把 l 移动到 重复字符的下一位
+      if (pos < i) {
+          ans = Math.max(ans, i - l);
+          l = pos + 1;
+      }
   }
-  return Math.max(max, arr.length)
+  return Math.max(ans, i - l);
 };
 
 const s = 'abcabcbb'
