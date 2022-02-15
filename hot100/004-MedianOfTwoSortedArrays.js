2b2a5f334b738bc67c12304bd23876d09fe08942
Lzh <nordglede@outlook.com>
2022-02-15 23:40:29 +0800

    004-2

--- hot100/004-MedianOfTwoSortedArrays.js
+++ hot100/004-MedianOfTwoSortedArrays.js
@@ -4,29 +4,52 @@
  * @return {number}
  */
 const findMedianSortedArrays = (nums1, nums2) => {
-  let i = 0, j = 0
-  const arr = []
-  while (nums1.length || nums2.length) {
-    if (nums1 && nums1[i] < nums2[j] || nums2.length === 0) {
-      arr.push(nums1.splice(i, 1)[0])
-    } else if (nums2 && nums2[j] < nums1[i] || nums1.length === 0) {
-      arr.push(nums2.splice(j, 1)[0])
-    } else {
-      arr.push(nums1.splice(i, 1)[0])
-      arr.push(nums2.splice(j, 1)[0])
-    }
+  const len1 = nums1.length;
+  const len2 = nums2.length;
+  const totalLen = len1 + len2;
+  if (totalLen % 2 === 1) {
+    const midLen = Math.floor(totalLen / 2);
+    return getMedianSortedArrays(nums1, nums2, midLen + 1);
+  } else {
+    const midLen1 = Math.floor(totalLen / 2) - 1;
+    const midLen2 = Math.floor(totalLen / 2);
+    return (getMedianSortedArrays(nums1, nums2, midLen1 + 1)  + getMedianSortedArrays(nums1, nums2, midLen2 + 1)) / 2;
   }
-  const len = arr.length
-  if (len % 2 === 0) {
-    return (arr[len / 2 - 1] + arr[len / 2]) / 2;
-  }
-  return arr[(len - 1) / 2];
 
 };
 
-const num1 = [0, 0];
-const num2 = [0, 0];
-console.log(findMedianSortedArrays(num1, num2));
+const getMedianSortedArrays = (nums1, nums2, k) => {
+  const len1 = nums1.length;
+  const len2 = nums2.length;
+  let index1 = 0
+  let index2 = 0
+  console.log(nums1,nums2,k);
+  while (true) {
+    // 边界
+    if (index1 == len1) return nums2[index2 + k - 1];
+    if (index2 == len2) return nums1[index1 + k - 1];
+    if (k === 1) return Math.min(nums1[index1], nums2[index2]);
+
+    // 正常情况
+    let half = Math.floor(k/2);
+    let newIndex1 = Math.min(index1 + half, len1) - 1;
+    let newIndex2 = Math.min(index2 + half, len2) - 1;
+    const pivot1 = nums1[newIndex1];
+    const pivot2 = nums2[newIndex2];
+    if (pivot1 <= pivot2) {
+      k -= (newIndex1 - index1 + 1);
+      index1 = newIndex1 + 1;
+    } else {
+      k -= (newIndex2 - index2 + 1);
+      index2 = newIndex2 + 1;
+    }
+  }
+};
+
+const nums1 = [1,2];
+// const nums1 = [2,3];
+const nums2 = [3,4];
+console.log(findMedianSortedArrays(nums1, nums2));
 // Input: nums1 = [1,3], nums2 = [2]
 // Output: 2.00000
 // Explanation: merged array = [1,2,3] and median is 2.
